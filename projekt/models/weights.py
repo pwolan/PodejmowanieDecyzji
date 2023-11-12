ad01e62c9f3d2a08234f4d66994f7be262aea7c3
KubaBiz <1kubi1@wp.pl>
2023-11-12 13:17:12 +0100

    poprawa bazy

--- projekt/models/weights.py
+++ projekt/models/weights.py
@@ -5,18 +5,14 @@ from .models import Criterias
 class Weights(models.Model):
     pass
 
-
-class WeightData(models.Model):
-    size = models.IntegerField()
-
-
-class DataElement(models.Model):
-    weightDataID = models.ForeignKey(WeightData, on_delete=models.CASCADE)
-    expertlDorXValue = models.IntegerField()
-    value = models.FloatField()
-
-
 class WeightsCriterias(models.Model):
     weightID = models.ForeignKey(Weights, on_delete=models.CASCADE)
     criteriaID = models.ForeignKey(Criterias, on_delete=models.CASCADE)
-    weightDataID = models.ForeignKey(WeightData, on_delete=models.CASCADE)
+    weightDataID = models.IntegerField(unique=True)
+    Size = models.IntegerField(default=0)
+
+
+class DataElements(models.Model):
+    weightDataID = models.ForeignKey(WeightsCriterias, on_delete=models.CASCADE, to_field="weightDataID")
+    expertlDorXValue = models.IntegerField()
+    value = models.FloatField()
