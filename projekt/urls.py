442b4221e79ed9da23957004eb6b9ce7872ca82b
jkr02 <jakubkroczek02@gmail.com>
2023-11-12 19:51:08 +0100

    some_paths

--- projekt/urls.py
+++ projekt/urls.py
@@ -3,7 +3,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('Model', GetModel, basename='model')
-router.register('Expert', GetExperts, basename='expert')
-router.register('ModelExpert', GetModelExpert, basename='modelExpert')
+router.register('model', GetModel, basename='model')
+router.register('expert', GetExperts, basename='expert')
+router.register('model_expert', GetModelExpert, basename='model_expert')
+router.register('criterias', GetCriterias, basename='criterias')
+router.register('model_criterias', GetModelCriterias, basename='model_criterias')
+router.register('weight_criterias', GetWeightsCriterias, basename='weight_criterias')
+router.register('data_elements', GetDataElements, basename='data_elements')
+router.register('matrices', GetMatrices, basename='matrices')
+router.register('matrice_elements', GetMatriceElements, basename='matrice_elements')
+router.register('data_matrices', GetDataMatrices, basename='data_matrices')
+router.register('decision_scenarios', GetDecisionScenarios, basename='decision_scenarios')
 urlpatterns = router.urls
--- projekt/views.py
+++ projekt/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import status
 from .models.models import *
+from .models.weights import *
+from .models.matrices import *
+from .models.decisionScenarios import *
 from .serializers import*
 from rest_framework.response import Response
 # Create your views here.
@@ -13,13 +16,46 @@ class GetModel(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         data = list(Models.objects.all().values())
         return Response(data)
+
 class GetExperts(viewsets.ModelViewSet):
     queryset = Experts.objects.all()
     serializer_class = ExpertSerializer
+
 class GetModelExpert(viewsets.ModelViewSet):
     queryset = ModelExperts.objects.all()
     serializer_class = ModelExpertSerializer
 
+class GetCriterias(viewsets.ModelViewSet):
+    queryset = Criterias.objects.all()
+    serializer_class = CriteriasSerializer
+
+class GetModelCriterias(viewsets.ModelViewSet):
+    queryset = ModelCriterias.objects.all()
+    serializer_class = ModelCriteriasSerializer
+
+class GetWeightsCriterias(viewsets.ModelViewSet):
+    queryset = WeightsCriterias.objects.all()
+    serializer_class = WeightsCriteriasSerializer
+
+class GetDataElements(viewsets.ModelViewSet):
+    queryset = DataElements.objects.all()
+    serializer_class = DataElementsSerializer
+
+class GetMatrices(viewsets.ModelViewSet):
+    queryset = Matrices.objects.all()
+    serializer_class = MatricesSerializer
+
+class GetMatriceElements(viewsets.ModelViewSet):
+    queryset = MatriceElements.objects.all()
+    serializer_class = MatriceElementsSerializer
+
+class GetDataMatrices(viewsets.ModelViewSet):
+    queryset = DataMatrices.objects.all()
+    serializer_class = DataMatricesSerializer
+
+class GetDecisionScenarios(viewsets.ModelViewSet):
+    queryset = DecisionScenarios.objects.all()
+    serializer_class = DecisionScenariosSerializer
 
 def main(request):
     template = loader.get_template('index.html')
