494e776ce1b3aa615b6d8d2f94dcdf5280f641c6
pwolan <wolanin.piotr1500@gmail.com>
2023-12-29 18:50:53 +0100

    css fixing

--- PodejmowanieDecyzji/urls.py
+++ PodejmowanieDecyzji/urls.py
@@ -25,6 +25,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("projekt.urls")),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
